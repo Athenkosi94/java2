8181f37abd87505cf497f4bac4293162bf2d4d74
user <athimbityana@gmail com>
2022-06-03 12:44:26 +0100

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i;
+        for(i=1; i<=50;i++)
+        {
+            System.out.println(i);
+        }
     }
 }
